e815cfae70f6df08e8c19d83d65d2ee60df46c4c
BSanjik <kundyz.sanjik@gmail.com>
2025-05-22 13:33:02 +0500

    webAppAddingTemplateFromBootstrap

--- FirstWebApp/www/main.go
+++ FirstWebApp/www/main.go
@@ -7,10 +7,11 @@ import (
 )
 
 type User struct {
-	name                 string
-	age                  uint16
-	money                int16
-	avgGrades, happiness float64
+	Name                 string
+	Age                  uint16
+	Money                int16
+	AvgGrades, Happiness float64
+	Hobbies              []string
 }
 
 func main() {
@@ -25,16 +26,16 @@ func handleRequest() {
 }
 
 func (u User) getAllInfo() string {
-	return fmt.Sprintf("User name is: %s. He is %d and he has money equal: %d", u.name, u.age, u.money)
+	return fmt.Sprintf("User name is: %s. He is %d and he has money equal: %d", u.Name, u.Age, u.Money)
 }
 
 func (u *User) setNewName(newName string) {
-	u.name = newName
+	u.Name = newName
 }
 
 // Home Page
 func homePage(w http.ResponseWriter, r *http.Request) {
-	bob := User{"Bob", 25, -50, 4.2, 0.8}
+	bob := User{"Bob", 25, -50, 4.2, 0.8, []string{"Football", "Site", "asd"}}
 	// bob.setNewName("Alex")
 	// fmt.Fprintf(w, "<b>Main Text</b>")
 	template, _ := template.ParseFiles("templates/homePage.html")
